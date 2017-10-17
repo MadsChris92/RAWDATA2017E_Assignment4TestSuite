71dc5d9905496042253f577732ff47b134a1af27
malaue4 <malaue4@gmail.com>
2017-10-17 19:59:09 +0200

    opsat det implicitte kode

--- Assignment4.Tests/DataServiceTests.cs
+++ Assignment4.Tests/DataServiceTests.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.Linq;
 
 namespace Assignment4.Tests
 {
--- Assignment4/Class1.cs
+++ Assignment4/Class1.cs
@@ -1,8 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Assignment4
 {
     public class DataService
     {
+        public Category GetCategory(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OrderDetails> GetOrderDetailsByOrderId(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OrderDetails> GetOrderDetailsByProductId(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class OrderDetails : List<Order>
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public double UnitPrice { get; set; }
+        public double Quantity { get; set; }
+        public double Discount { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Required { get; set; }
+        public OrderDetails OrderDetails { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCity { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        //public double QuantityPerUnit { get; set; }
+        public int UnitsInStock { get; set; }
+        public Category Category { get; set; }
     }
 }
