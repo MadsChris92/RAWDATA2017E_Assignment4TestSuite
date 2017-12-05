3e7fdb79a143c130d8d300511a4e95968b863166
malaue4 <malaue4@gmail.com>
2017-12-05 18:35:21 +0100

    sparkling streams

--- WebService/wwwroot/main.js
+++ WebService/wwwroot/main.js
@@ -1,34 +1,20 @@
 ﻿requirejs.config({
-    baseUrl: 'lib',
+    baseUrl: "scripts",
     paths: {
-        knockout: 'knockout/dist/knockout',
-        bootstrap: 'bootstrap/dist/js/bootstrap'
+        knockout: "../lib/knockout/dist/knockout",
+        bootstrap: "../lib/bootstrap/dist/js/bootstrap",
+        jquery: "../lib/jquery/dist/jquery",
+        text: "../lib/text/text",
+        jqcloud: "../lib/jqcloud2/dist/jqcloud"
     }
-})
+});
 
-require(['knockout'], function (ko) {
+require(['knockout', 'jquery'], function (ko, $) {
     var vm = (function () {
     
 
         return {
-            test: ko.observable("GSEDGKWSD"),
-            postListArray: ko.observableArray([]),
-            resultArray: ko.observableArray([]),
-            searchWord: ko.observable(""),
-
-            getPostList: function () {
-                $.ajax({
-                    url: "http://localhost:5001/api/posts/title/" + vm.searchWord,
-                    method: "GET",
-                    dataType: "json",
-                    success: function (data) {
-                        vm.postListArray(data);
-                        vm.resultArray(data.results);
-                        console.log(data);
-                    }
-                })
-            }
-        };
+        }
     })();
 
     ko.applyBindings(vm);
