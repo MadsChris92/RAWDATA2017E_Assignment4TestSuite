3d67cc7ea86f83a6fb51f5c0710af3c9e7746209
MadsChris92 <madschris888@gmail.com>
2017-12-05 14:37:43 +0100

    web setup

---
+++ WebService/wwwroot/main.js
@@ -0,0 +1,19 @@
+﻿requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        knockout: 'knockout/dist/knockout',
+        bootstrap: 'bootstrap/dist/js/bootstrap'
+    }
+})
+
+require(['knockout'], function (ko) {
+    var vm = (function () {
+
+
+        return {
+            test: ko.observable("GSEDGKWSD")
+        };
+    })();
+
+    ko.applyBindings(vm);
+});
