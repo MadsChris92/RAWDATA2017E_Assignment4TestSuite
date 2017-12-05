8e065887072b34f3e2053c2c0af8bc3d54e2e956
MadsChris92 <madschris888@gmail.com>
2017-12-05 18:36:49 +0100

    jklk

--- WebService/wwwroot/main.js
+++ WebService/wwwroot/main.js
@@ -2,29 +2,34 @@
     baseUrl: 'lib',
     paths: {
         knockout: 'knockout/dist/knockout',
-        bootstrap: 'bootstrap/dist/js/bootstrap'
+        bootstrap: 'bootstrap/dist/js/bootstrap',
+        jquery: "../lib/jquery/dist/jquery",
+        text: "../lib/text/text",
+        jqcloud: "../lib/jqcloud2/dist/jqcloud"
     }
 })
 
-require(['knockout'], function (ko) {
+require(["knockout", "jquery"], function (ko, $) {
     var vm = (function () {
-    
+        searchWord: ko.observable("")
 
         return {
             test: ko.observable("GSEDGKWSD"),
             postListArray: ko.observableArray([]),
             resultArray: ko.observableArray([]),
-            searchWord: ko.observable(""),
-
+            searchWord: this.searchWord,
+            
             getPostList: function () {
                 $.ajax({
                     url: "http://localhost:5001/api/posts/title/" + vm.searchWord,
                     method: "GET",
                     dataType: "json",
                     success: function (data) {
+
                         vm.postListArray(data);
                         vm.resultArray(data.results);
                         console.log(data);
+                        console.log(vm.searchWord);
                     }
                 })
             }
