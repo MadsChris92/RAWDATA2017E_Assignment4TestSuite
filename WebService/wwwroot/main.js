be87c21bbcbc8a22ce99760c01c05ec1bbba498d
Martin Laursen <malaue4@gmail.com>
2017-12-06 17:13:48 +0100

    duplicate

--- WebService/wwwroot/main.js
+++ WebService/wwwroot/main.js
@@ -7,8 +7,47 @@
         text: "../lib/text/text",
         jqcloud: "../lib/jqcloud2/dist/jqcloud",
         dataservice: "../scripts/services/dataservice"
+    },
+    shim: {
+        jqcloud: {
+            deps:['jquery']
+        }
     }
-})
+});
+
+require(['knockout', 'jquery', 'jqcloud'], function (ko, $) {
+    ko.bindingHandlers.cloud = {
+        init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+            // This will be called when the binding is first applied to an element
+            // Set up any initial state, event handlers, etc. here
+            var words = allBindings.get('cloud').words;
+            if (words && ko.isObservable(words)) {
+                words.subscribe(function () {
+                    $(element).jQCloud('update', ko.unwrap(words));
+                });
+            }
+            console.log("in init: ");
+        },
+        update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+            console.log("in update: ");
+            // This will be called once when the binding is first applied to an element,
+            // and again whenever any observables/computeds that are accessed change
+            // Update the DOM element based on the supplied values here.
+            var words = ko.unwrap(allBindings.get('cloud').words) || [];
+            $(element).jQCloud(words);
+        }
+    };
+});
+
+
+require(["knockout"], function (ko) {
+    ko.components.register("wordCloud",
+        {
+            viewModel: { require: "../scripts/components/wordCloud/wordCloud" },
+            template: { require: "text!../scripts/components/wordCloud/wordCloud.html" }
+        }
+    );
+});
 
 require(["knockout", "jquery", "dataservice"], function (ko, $, dat) {
     var vm = (function() {
