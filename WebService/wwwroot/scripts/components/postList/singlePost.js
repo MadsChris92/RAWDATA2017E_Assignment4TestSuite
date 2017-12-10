d990d45850873dd6c6662b1ec2f64b5e01653c84
malaue4 <malaue4@gmail.com>
2017-12-10 04:03:45 +0100

    Hidden comments, hidden with a comment

--- WebService/wwwroot/scripts/components/postList/singlePost.js
+++ WebService/wwwroot/scripts/components/postList/singlePost.js
@@ -3,12 +3,16 @@
         var self = this;
         
         var postResult = params.name;
+        var hasComments = ko.computed(function() {
+            return typeof postResult().comments !== "undefined" && postResult().comments.length > 0;
+        }, this);
         //var postResult = ko.observable(params.name || '');
 
         
         console.log(postResult);
         return {
-            postResult
+            postResult,
+            hasComments
         };
 
     }
